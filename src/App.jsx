88dc046518ee2986eb4971df72e53201d658148f
salvatore monggiotti tassinari <18350507@gmail.com>
2022-03-09 12:27:14 -0600

    unico commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import  Saludos from "./Saludos";
+function App() {
+  return (
+    <div className="App">
+      <Saludos />
+      </div>
+  );
+
+}
+
+export default App;
---
+++ src/Saludos.jsx
@@ -0,0 +1,6 @@
+const Saludos = ()=> {
+   
+    return 
+        <h1>este es mi segundo intento de ser piloto de f1 en ReactJS!!</h1>
+     export default  Saludos;
+}
